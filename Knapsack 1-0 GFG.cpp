240d4db18b67d189d012dfc2cc2e728ec0929626
Mongoose2004 <147995982+Mongoose2004@users.noreply.github.com>
2024-01-07 15:36:13 +0530

    Add files via upload

---
+++ Knapsack 1-0 GFG.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int find(int index, int W, int wt[],int val[],vector<vector<int>>&dp)
+      {  
+          //base condition
+         if(W==0)
+           return 0;
+           if(index==0)
+            return 0;
+            if(dp[index][W]!=-1)
+             return dp[index][W];
+           if(wt[index-1]>W)
+            return dp[index][W]=find(index-1,W,wt,val,dp);
+            else
+            return dp[index][W]=max(val[index-1]+find(index-1,W-wt[index-1],wt,val,dp),find(index-1,W,wt,val,dp));
+          
+        
+      }
+    int knapSack(int W, int wt[], int val[], int n) 
+    { vector<vector<int>>dp(n+1,vector<int>(W+1,-1));
+        return find(n,W,wt,val,dp);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout<<ob.knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}
